fa3f0cac1a0edb4d7878380e95e5863ac8cb0124
Rajat <rajat@tiobu.com>
2023-06-20 05:37:31 +0530

    Improved Lazy loading

--- pare-frontend/src/components/custom/Img.jsx
+++ pare-frontend/src/components/custom/Img.jsx
@@ -1,6 +1,8 @@
 import { LazyLoadImage } from 'react-lazy-load-image-component'
 const Img = ({ alt, src, height, width, className, ...props }) => {
-  return <LazyLoadImage alt={alt} height={height} src={src} width={width} className={className} {...props} />
+  return (
+    <LazyLoadImage alt={alt} height={height} src={src} width={width} className={className} threshold={300} {...props} />
+  )
 }
 
 export default Img
