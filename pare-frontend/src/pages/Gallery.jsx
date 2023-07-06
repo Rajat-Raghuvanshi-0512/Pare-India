61b1e2d98736b075e73a27146f241459a767cb79
Rajat <rajat.karnal@gmail.com>
2023-07-06 16:58:51 +0530

    converted png to webp

--- pare-frontend/src/pages/Gallery.jsx
+++ pare-frontend/src/pages/Gallery.jsx
@@ -1,11 +1,13 @@
+import { Suspense } from 'react'
 import { GalleryMain, GallerySection } from '../components/gallery'
+import Loader from '../components/Loader'
 
 const Gallery = () => {
   return (
-    <>
+    <Suspense fallback={<Loader />}>
       <GalleryMain />
       <GallerySection />
-    </>
+    </Suspense>
   )
 }
 
