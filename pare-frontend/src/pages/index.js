4c44b7851525927658c8b10dfd69b1052b6c36ed
Rajat <rajat.karnal@gmail.com>
2023-07-07 20:49:25 +0530

    Removed lazyloading pages

--- pare-frontend/src/pages/index.js
+++ pare-frontend/src/pages/index.js
@@ -1,16 +1,15 @@
-import { lazy } from 'react'
 import NotFound from './404'
 
 import Home from './Home'
-const About = lazy(() => import('./About'))
-const Contact = lazy(() => import('./Contact'))
-const Products = lazy(() => import('./Products'))
-const Careers = lazy(() => import('./Careers'))
-const CareerForm = lazy(() => import('./CareerForm'))
-const Product1 = lazy(() => import('./Product1'))
-const Linea = lazy(() => import('./Linea'))
-const FAQ = lazy(() => import('./FAQ'))
-const Resources = lazy(() => import('./Resources'))
-const Gallery = lazy(() => import('./Gallery'))
+import About from './About'
+import Contact from './Contact'
+import Products from './Products'
+import Careers from './Careers'
+import CareerForm from './CareerForm'
+import Product1 from './Product1'
+import Linea from './Linea'
+import FAQ from './FAQ'
+import Resources from './Resources'
+import Gallery from './Gallery'
 
 export { Home, About, Contact, Products, Careers, CareerForm, Product1, Linea, FAQ, NotFound, Resources, Gallery }
